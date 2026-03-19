da0b1db425e56f80e03b6be276d0aff8c267b163
jeffy-d-martin <jeffydmartin@gmail.com>
2026-03-19 21:59:38 +0530

    Recursion ......

---
+++ Day_1/factorial.py
@@ -0,0 +1,7 @@
+def factorial( num ) :
+    if num == 1 or num == 0 :
+        return 1
+    else :
+        return num * factorial( num - 1 )
+num = 5
+print(f"The Factorial of {num} is {factorial(num)}")
---
+++ Day_1/printNterms.py
@@ -0,0 +1,8 @@
+def prime(number):
+    if sum == number : return number
+    print(sum)
+    sum += 1
+
+
+
+number = input("Enter the number : ")
